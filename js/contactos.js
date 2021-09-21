886ee22511a83308a89555cea4471f6afe1568a1
mrjhong <jfdg206@gmail.com>
2021-09-21 14:47:04 -0500

    validaciones

--- js/contactos.js
+++ js/contactos.js
@@ -1,12 +1,26 @@
-function validar(){
-    nombre=document.getElementById("name").value;
-    apellido=document.getElementById("LastName").value;
+function validar() {
+    nombre = document.getElementById("name").value;
+    apellido = document.getElementById("LastName").value;
+    correo = document.getElementById("email").value;
+    formato = /\w+@\w+\.+[a-z]/;
+    if (nombre.length == 0 || apellido.length == 0
+        || /^\s+$/.test(apellido, nombre,)) {
+        alert("Todos Los Campos Son Obligatorios");
+        return false;
+    }
+    else if (nombre.length > 30) {
+        alert("El Nombre Es Muy Largo");
+        return false;
 
-    if( nombre.length==0 || /^\s+$/.test(nombre)){
-        alert("no puedes dejar el campo Nombre vacio");
     }
-    if( apellido.length==0 || /^\s+$/.test(apellido)){
-        alert("no puedes dejar el campo Apellido vacio");
+
+    else if (apellido.length > 30) {
+        alert("El Apellido es muy Largo");
+        return false;
     }
- 
+    else if (!formato.test(correo)) {
+        alert("Correo Incorrecto");
+        return false;
+    }
+
 }
