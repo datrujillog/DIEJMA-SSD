9d8e5bfb37841ebd9b6559a2cf142f9ca3b83d38
yego0210 <diegotrujillo0210@gmail.com>
2021-10-03 20:36:57 -0500

    Diego - - Trujillo - - Productos

--- js/index.js
+++ js/index.js
@@ -113,3 +113,28 @@ countdwn('Oct 31 2021 17:10:00 GMT-0500', 'clock',
 
 // el.innerHTML = ` ${t.remainDays}d:${t.remainHours}h:${t.remainMinutes}m:${t.remainSeconds}s`;
 // ******************** FIN CODIGO ********************
+
+
+// ================= BOOTON ========================
+// function validar() {
+//     nombre = document.getElementById("name").value;
+//     apellido = document.getElementById("LastName").value;
+//     correo = document.getElementById("email").value;
+//     formato = /\w+@\w+\.+[a-z]/;
+//     if (nombre.length == 0 || apellido.length == 0 ||
+//         /^\s+$/.test(apellido, nombre, )) {
+//         alert("Todos Los Campos Son Obligatorios");
+//         return false;
+//     } else if (nombre.length > 30) {
+//         alert("El Nombre Es Muy Largo");
+//         return false;
+
+//     } else if (apellido.length > 30) {
+//         alert("El Apellido es muy Largo");
+//         return false;
+//     } else if (!formato.test(correo)) {
+//         alert("Correo Incorrecto");
+//         return false;
+//     }
+
+// }
