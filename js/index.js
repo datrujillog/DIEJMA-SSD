3c97a13e7c7fe1404c0990152b34a59717dc41e9
yego0210 <diegotrujillo0210@gmail.com>
2021-10-05 17:32:42 -0500

    Diego - - Trujillo - - Pagina inicio

--- js/index.js
+++ js/index.js
@@ -54,22 +54,26 @@ window.addEventListener('load', () => {
 // ******************** Cuenta regresiva  ********************
 
 const getRemainTime = deadline => {
-    let now = new Date(),
-        remainTime = (new Date(deadline) - now + 1000) / 1000,
-        remainSeconds = ('0' + Math.floor(remainTime % 60)).slice(-2),
-        remainMinutes = ('0' + Math.floor(remainTime / 60 % 60)).slice(-2),
-        remainHours = ('0' + Math.floor(remainTime / 3600 % 24)).slice(-2);
-    remainDays = Math.floor(remainTime / (3600 * 24));
+        let now = new Date(),
+            remainTime = (new Date(deadline) - now + 1000) / 1000,
+            remainSeconds = ('0' + Math.floor(remainTime % 60)).slice(-2),
+            remainMinutes = ('0' + Math.floor(remainTime / 60 % 60)).slice(-2),
+            remainHours = ('0' + Math.floor(remainTime / 3600 % 24)).slice(-2);
+        remainDays = Math.floor(remainTime / (3600 * 24));
 
-    return {
-        remainTime,
-        remainSeconds,
-        remainMinutes,
-        remainHours,
-        remainDays
+        return {
+            remainTime,
+            remainSeconds,
+            remainMinutes,
+            remainHours,
+            remainDays
+        }
+
     }
-
-}
+    // <div style="font-size: 25px;">D</div>
+    // <br><div style="font-size: 25px;">H</div>
+    // <br> <div style="font-size: 25px;">Minutes</div>
+    // <br><div style="font-size: 25px;">Seconds</div></div>  `
 
 // console.log(getRemainTime('Sep 22 2021 15:00:51 GMT-0500 '))
 
@@ -80,18 +84,20 @@ const countdwn = (deadline, elem, finalMessage) => {
 
         let t = getRemainTime(deadline);
 
-        el.innerHTML = `<div style="background-color: rgba(0, 0, 0, 0.75); width: 12%; height: 150px; border-radius: 50px;">${t.remainDays}<br> <div style="font-size: 25px;">Days</div>
+        el.innerHTML = `<div class=" col" style="background-color: rgba(0, 0, 0, 0.75); width: 10px; height: 100px; border-radius: 50px;">${t.remainDays} D:
+        
          </div> 
         
         
-       <div style="background-color: rgba(0, 0, 0, 0.75); width: 12%; height: 150px; border-radius: 50px; ">${t.remainHours}<br><div style="font-size: 25px;">Hours</div>
+       <div class="col" style="background-color: rgba(0, 0, 0, 0.75); width: 12%; height: 100px; border-radius: 50px; ">${t.remainHours} H:
        </div> 
         
         
-        <div style="background-color: rgba(0, 0, 0, 0.75); width: 13%; height: 150px; border-radius: 50px;">${t.remainMinutes}<br> <div style="font-size: 25px;">Minutes</div>
+        <div class="col"  style="background-color: rgba(0, 0, 0, 0.75); width: 12%; height: 100px; border-radius: 50px;">${t.remainMinutes} M:
         </div>
         
-       <div style="background-color: rgba(0, 0, 0, 0.75); width: 14%; height: 150px; border-radius: 50px;">${t.remainSeconds}<br><div style="font-size: 25px;">Seconds</div></div>  `;
+       <div class=" col" style="background-color: rgba(0, 0, 0, 0.75); width: 12%; height: 100px; border-radius: 50px;">${t.remainSeconds} S:
+       </div>`;
 
 
 
@@ -107,34 +113,3 @@ const countdwn = (deadline, elem, finalMessage) => {
 
 countdwn('Oct 31 2021 17:10:00 GMT-0500', 'clock',
     'Feliz 2021');
-
-// style="background-color: rgba(0, 0, 0, 0.4); width: 100px; height: 100px;  border-radius: 50px; !import"
-
-
-// el.innerHTML = ` ${t.remainDays}d:${t.remainHours}h:${t.remainMinutes}m:${t.remainSeconds}s`;
-// ******************** FIN CODIGO ********************
-
-
-// ================= BOOTON ========================
-// function validar() {
-//     nombre = document.getElementById("name").value;
-//     apellido = document.getElementById("LastName").value;
-//     correo = document.getElementById("email").value;
-//     formato = /\w+@\w+\.+[a-z]/;
-//     if (nombre.length == 0 || apellido.length == 0 ||
-//         /^\s+$/.test(apellido, nombre, )) {
-//         alert("Todos Los Campos Son Obligatorios");
-//         return false;
-//     } else if (nombre.length > 30) {
-//         alert("El Nombre Es Muy Largo");
-//         return false;
-
-//     } else if (apellido.length > 30) {
-//         alert("El Apellido es muy Largo");
-//         return false;
-//     } else if (!formato.test(correo)) {
-//         alert("Correo Incorrecto");
-//         return false;
-//     }
-
-// }
